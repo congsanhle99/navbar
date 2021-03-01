a7783f21fb8a2ad34957d3c170e8c48707f4e761
congsanhle99 <67894202+congsanhle99@users.noreply.github.com>
2021-03-01 12:31:17 +0700

    second commit

---
+++ test.js
@@ -0,0 +1,17 @@
+// load nội dung trước
+document.addEventListener("DOMContentLoaded", function() {
+    var x = document.getElementById('follow');
+    
+    x.onclick = function() {
+        alert("Thanks you!!!");
+    }
+
+    var x1 = document.getElementById('mess');
+    x1.onclick = function() {
+        alert("Go to CongSanh message!!!")
+    }
+}, false)
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // code thực hiện trong này
+// }, false)
